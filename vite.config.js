c8a4c15128524bf65f1accd8be5783ba9dab7b97
aftabdivan-matrix <aftab.divan@matrixbrains.com>
2024-01-24 17:06:21 +0530

    deploying

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/aftabdivan-portfolio/',
-})
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/aftabdivan-portfolio/";
+  }
+
+  return config;
+});
